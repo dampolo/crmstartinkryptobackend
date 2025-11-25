d4a0a0bf529569185b77ef81311bbdf3f39de781
Damian Poloczek <damianpoloczek@gmail.com>
2025-11-25 22:08:30 +0100

    Company first data in DB

--- customer_app/api/serializer.py
+++ customer_app/api/serializer.py
@@ -1,7 +1,47 @@
 from rest_framework import serializers
-from customer_app.models import Customer
+from customer_app.models import Customer, CustomerComment
+
+
+class CustomerCommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomerComment
+        fields = ['id', 'text', 'created_at', 'updated_at']
+        read_only_fields = ['id', 'created_at', 'updated_at']
 
 class CustomerSerializer(serializers.ModelSerializer):
+    comments = CustomerCommentSerializer(many=True, required=False)
+
     class Meta:
         model = Customer
-        fields = '__all__'
+        fields = [
+            'id',
+            'photo',
+            'customer_number',
+            'title',
+            'first_name',
+            'last_name',
+            'street',
+            'number',
+            'post_code',
+            'city',
+            'email',
+            'phone',
+            'has_portfolio',
+            'has_subscription',
+            'is_active',
+            'created_at',
+            'updated_at',
+            'comments'
+        ]
+
+        read_only_fields = ['created_at', 'updated_at']
+
+    def create(self, validated_data):
+        comments_data = validated_data.pop('comments', [])
+
+        customer = Customer.objects.create(**validated_data)
+
+        for comment in comments_data:
+            CustomerComment.objects.create(customer=customer, **comment)
+
+        return customer
--- customer_app/api/views.py
+++ customer_app/api/views.py
@@ -1,7 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
-from .serializer import CustomerSerializer
-from customer_app.models import Customer
+from .serializer import CustomerSerializer, CustomerCommentSerializer
+from customer_app.models import Customer, CustomerComment
+
 
 class CustomerView(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class CustomerCommentViewSet(ModelViewSet):
+    serializer_class = CustomerCommentSerializer
+
+    def get_queryset(self):
+        return CustomerComment.objects.filter(customer_id=self.kwargs.get('customer_pk'))
+
+    def perform_create(self, serializer):
+        serializer.save(customer_id=self.kwargs.get('customer_pk'))
