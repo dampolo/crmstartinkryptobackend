b5ca7bed12a368067af50104b5f4c69b18ba67b8
Damian Poloczek <damianpoloczek@gmail.com>
2025-12-06 22:36:58 +0100

    company fetch works

--- crm_core/settings.py
+++ crm_core/settings.py
@@ -70,6 +70,8 @@ CSRF_TRUSTED_ORIGINS = [
 
     'http://127.0.0.1:5500',
 
+    'http://localhost:4200',
+
     'http://localhost:5500',
 
     'http://localhost:5173',
@@ -78,11 +80,13 @@ CSRF_TRUSTED_ORIGINS = [
 
 ]
 
-
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
 
     'http://127.0.0.1:5500',
+
+    'http://localhost:4200',
 
     'http://localhost:5500',
 
