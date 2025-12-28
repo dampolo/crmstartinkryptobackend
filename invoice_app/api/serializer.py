d2e5ff9c6f6577507b3de22bc13a2b23d497b675
Damian Poloczek <damianpoloczek@gmail.com>
2025-12-28 20:44:51 +0100

    repalce

--- invoice_app/api/serializer.py
+++ invoice_app/api/serializer.py
@@ -4,7 +4,7 @@ from invoice_app.models import Invoice, InvoiceService, ServiceCatalog
 class InvoiceServiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = InvoiceService
-        fields = ["id", "service_name", "provision_type", "provision_fixed", "provision_percent", "amount"]
+        fields = ['id', 'service_name', 'provision_type', 'provision_fixed', 'provision_percent', 'amount']
 
 class InvoiceSerializer(serializers.ModelSerializer):
     services = InvoiceServiceSerializer(many=True, read_only= True)
@@ -12,58 +12,58 @@ class InvoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Invoice
         fields = [
-            "id", "invoice_number","invoice_type", "invoice_status",
+            'id', 'invoice_number','invoice_type', 'invoice_status',
 
             # customer snapshot
-            "customer_name", "customer_address",
+            'customer_name', 'customer_address',
 
             # company snapshot
-            "company_name", "company_street", "company_number", "company_postcode",
-            "company_city", "company_tax_number", "company_email", "company_bank",
-            "company_bank_account", "company_swift_code", "company_logo",
+            'company_name', 'company_street', 'company_number', 'company_postcode',
+            'company_city', 'company_tax_number', 'company_email', 'company_bank',
+            'company_bank_account', 'company_swift_code', 'company_logo',
 
             # invoice totals
-            "provision", "amount", "investitions_amount", "value_tax",
+            'provision', 'amount', 'investitions_amount', 'value_tax',
 
             # relations
-            "customer", "user",
+            'customer', 'user',
 
             # system fields
-            "pdf_file", "created_at", "updated_at", "is_finalized",
+            'pdf_file', 'created_at', 'updated_at', 'is_finalized',
 
             # SERVICES LAST
-            "services"
+            'services'
         ]
         # Read-only fields: CANNOT be set by frontend
         read_only_fields = [
-            "id",
-            "user",                     # filled from request.user
-            "invoice_status",           # default = unpaid
-            "created_at",
-            "updated_at",
-            "is_finalized",
-            "pdf_file",
+            'id',
+            'user',                     # filled from request.user
+            'invoice_status',           # default = unpaid
+            'created_at',
+            'updated_at',
+            'is_finalized',
+            'pdf_file',
 
             # Customer snapshot
-            "customer_name",
-            "customer_address",
+            'customer_name',
+            'customer_address',
 
             # Company snapshot
-            "company_name",
-            "company_street",
-            "company_number",
-            "company_postcode",
-            "company_city",
-            "company_tax_number",
-            "company_email",
-            "company_bank",
-            "company_bank_account",
-            "company_swift_code",
-            "company_logo",
+            'company_name',
+            'company_street',
+            'company_number',
+            'company_postcode',
+            'company_city',
+            'company_tax_number',
+            'company_email',
+            'company_bank',
+            'company_bank_account',
+            'company_swift_code',
+            'company_logo',
             'services',
         ]
 
 class ServiceCatalogSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServiceCatalog
-        fields = ["name", "provision_type", "amount_fixed", "amount_percent"]
+        fields = ['name', 'provision_type', 'amount_fixed', 'amount_percent']
