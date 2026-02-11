fda1eec124d2ffa99501bc30633fde775e1b1ca0
Damian Poloczek <damianpoloczek@gmail.com>
2026-02-11 10:08:58 +0100

    course_app admin

--- course_app/admin.py
+++ course_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from course_app.models import Course, CourseFeature, Lesson, LessonPDF, DiscountCode
+
+admin.site.register(Course)
+admin.site.register(CourseFeature)
+admin.site.register(Lesson)
+admin.site.register(LessonPDF)
+admin.site.register(DiscountCode)
 
 # Register your models here.
