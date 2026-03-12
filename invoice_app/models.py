529965cfe3bb8de136758f7d64ecd376be74b494
Damian Poloczek <damianpoloczek@gmail.com>
2026-03-12 21:04:59 +0100

    notes_template

--- invoice_app/models.py
+++ invoice_app/models.py
@@ -19,6 +19,13 @@ class InvoiceCategory(models.TextChoices):
     COURSE = "course", _("Course")
     SERVICE = "service", _("Service")
 
+class TextSnippet(models.Model):
+    key = models.CharField(max_length=100, unique=True)
+    text = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.key
+
 class ServiceCatalog(models.Model):
 
     service_name = models.CharField(max_length=200)
@@ -142,6 +149,8 @@ class Invoice(models.Model):
     provision = models.DecimalField(decimal_places=2, max_digits=10)
     value_tax = models.DecimalField(decimal_places=2, max_digits=10)
     value_tax_amount = models.DecimalField(decimal_places=2, max_digits=10)
+
+    notes_template = models.TextField(blank=True)
 
     notes = models.TextField(blank=True)
 
