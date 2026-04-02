480df2f72970025cf52bfa3cc04aaf0339a5f75f
Damian Poloczek <damianpoloczek@gmail.com>
2026-04-02 12:03:59 +0200

    change url

--- course_app/api/urls.py
+++ course_app/api/urls.py
@@ -5,10 +5,10 @@ from course_app.api import views
 router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 router.register(r'courses-features', CourseFeatureViewSet, basename='courses-features')
-router.register(r"lessons", LessonViewSet, basename="lessons")
-router.register(r"purchases", PurchasedViewSet, basename="purchases")
-router.register(r"discount-codes", DiscountCodeViewSet, basename="discount-code")
-router.register(r"lesson-pdfs", LessonViewSet, basename="lesson-pdfs")
+router.register(r'lessons', LessonViewSet, basename='lessons')
+router.register(r'purchases', PurchasedViewSet, basename='purchases')
+router.register(r'discount-codes', DiscountCodeViewSet, basename='discount-code')
+router.register(r'lesson-pdfs', LessonViewSet, basename='lesson-pdfs')
 
 # CRM part
 router.register(r'crm-lessons', views.LessonViewSetCrmAPI, basename='crm-lessons')
