bb6d0f11915ead998553b914c8c5195c3f7afb50
Damian Poloczek <damianpoloczek@gmail.com>
2026-04-04 22:49:48 +0200

    Update LessonViewSetCrmAPI

--- course_app/api/views.py
+++ course_app/api/views.py
@@ -40,13 +40,14 @@ class CourseFeatureViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
 
 class LessonViewSetCrmAPI(viewsets.ModelViewSet):
+    queryset = Lesson.objects.all()
     serializer_class = LessonSerializerCrm
 
     def get_queryset(self):
         course = self.request.query_params.get('course')
-        return Lesson.objects.filter(
-            course=course,
-        )
+        if course:
+            return Lesson.objects.filter(course=course)
+        return Lesson.objects.all()
 
 
 class LessonViewSet(viewsets.ModelViewSet):
