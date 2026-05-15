23e595f57318360373a840367d22e434e16a87c0
Damian Poloczek <damianpoloczek@gmail.com>
2026-05-15 19:43:46 +0200

    course_app update urls

--- course_app/api/urls.py
+++ course_app/api/urls.py
@@ -8,7 +8,6 @@ router.register(r'courses-features', CourseFeatureViewSet, basename='courses-fea
 router.register(r'lessons', LessonViewSet, basename='lessons')
 router.register(r'purchases', PurchasedViewSet, basename='purchases')
 router.register(r'discount-codes', DiscountCodeViewSet, basename='discount-code')
-# router.register(r'lesson-pdfs', LessonViewSet, basename='lesson-pdfs')
 
 # CRM part
 router.register(r'crm-lessons', views.LessonViewSetCrmAPI, basename='crm-lessons')
