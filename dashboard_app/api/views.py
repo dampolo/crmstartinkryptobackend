abbc7020a4230bdc0f0b4a84b9eae2444df49604
Damian Poloczek <damianpoloczek@gmail.com>
2026-06-01 21:23:12 +0200

    lates_inovices and latest_customers

--- dashboard_app/api/views.py
+++ dashboard_app/api/views.py
@@ -8,8 +8,32 @@ class DashboardCrmAPIView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def get(self, request):
+        latest_customers = (
+            User.objects
+            .filter(role=User.ProfileType.CUSTOMER)
+            .order_by('-updated_at')[:3]
+            .values(
+                'first_name',
+                'last_name',
+                'email',
+                'has_portfolio',
+            )
+        ),
+        latest_invoices = (
+            Invoice.objects
+            .order_by('-updated_at')[:3]
+            .values(
+                'invoice_number',
+                'user_customer_first_name',
+                'user_customer_last_name',
+                'invoice_status',
+                'amount'
+            )
+        )
         return Response({
             'customers_count': User.objects.filter(role=User.ProfileType.CUSTOMER).count(),
             'applicants_count': User.objects.filter(role=User.ProfileType.APPLICANT).count(),
-            'invioces_count': Invoice.objects.count()
+            'invioces_count': Invoice.objects.count(),
+            'latest_customers': list(latest_customers),
+            'latest_invoices': list(latest_invoices)
         })
